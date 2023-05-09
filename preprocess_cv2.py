365eb3b3ca9193372f7029aa40645cce443944a2
Jaeha Song <82474794+aarchiiive@users.noreply.github.com>
2023-05-09 17:22:13 +0900

    Update preprocess_cv2.py
    Add robustness about detection cases

--- preprocess_cv2.py
+++ preprocess_cv2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import datetime
+from re import A
 from matplotlib import image
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import random
@@ -102,8 +103,10 @@ def pipeline(img, s_thresh=(100, 255), sx_thresh=(15, 255)):
 
 def perspective_warp(img,
                      dst_size=(320, 180),
-                     src=np.float32([(0.395, 0.58), (0.605, 0.58), (0.06, 0.75), (0.94, 0.75)]),
+                     src=np.float32([(0.28, 0.63), (0.72, 0.63), (0.01, 0.75), (0.99, 0.75)]),
                      dst=np.float32([(0, 0), (1, 0), (0, 1), (1, 1)])):
+                    # (0.35, 0.63), (0.65, 0.63), (0.08, 0.75), (0.92, 0.75) alomst best but not in highly curved lane
+                    # (0.395, 0.58), (0.605, 0.58), (0.06, 0.75), (0.94, 0.75)
     # img = region(img)
     img_size = np.float32([(img.shape[1], img.shape[0])])
     src = src * img_size
@@ -121,7 +124,8 @@ def perspective_warp(img,
 def inv_perspective_warp(img,
                          dst_size=(320, 180),
                          src=np.float32([(0, 0), (1, 0), (0, 1), (1, 1)]),
-                         dst=np.float32([(0.415, 0.58), (0.585, 0.58), (0.06, 0.75), (0.94, 0.75)])):
+                         dst=np.float32([(0.28, 0.63), (0.72, 0.63), (0.01, 0.75), (0.99, 0.75)])):
+                                        # (0.415, 0.58), (0.585, 0.58), (0.06, 0.75), (0.94, 0.75)
                                         # (0.36, 0.54), (0.64, 0.54), (0, 0.85), (1, 0.85)
                                         # 0.35, 0.55
     img_size = np.float32([(img.shape[1], img.shape[0])])
@@ -141,7 +145,7 @@ def get_hist(img):
     hist = np.sum(img[img.shape[0]//2:, :], axis=0)
     return hist
 
-def sliding_window(img, nwindows=9, margin=80, minpix=1, draw_windows=True):
+def sliding_window(img, nwindows=9, margin=40, minpix=1, draw_windows=True):
     # global left_a, left_b, left_c,right_a, right_b, right_c
     left_a, left_b, left_c = [], [], []
     right_a, right_b, right_c = [], [], []
@@ -198,17 +202,7 @@ def sliding_window(img, nwindows=9, margin=80, minpix=1, draw_windows=True):
             leftx_current = int(np.mean(nonzerox[good_left_inds]))
         if len(good_right_inds) > minpix:
             rightx_current = int(np.mean(nonzerox[good_right_inds]))
-
-
-#        if len(good_right_inds) > minpix:
-#            rightx_current = np.int(np.mean([leftx_current +900, np.mean(nonzerox[good_right_inds])]))
-#        elif len(good_left_inds) > minpix:
-#            rightx_current = np.int(np.mean([np.mean(nonzerox[good_left_inds]) +900, rightx_current]))
-#        if len(good_left_inds) > minpix:
-#            leftx_current = np.int(np.mean([rightx_current -900, np.mean(nonzerox[good_left_inds])]))
-#        elif len(good_right_inds) > minpix:
-#            leftx_current = np.int(np.mean([np.mean(nonzerox[good_right_inds]) -900, leftx_current]))
-
+            
     # Concatenate the arrays of indices
     left_lane_inds = np.concatenate(left_lane_inds)
     right_lane_inds = np.concatenate(right_lane_inds)
@@ -219,48 +213,194 @@ def sliding_window(img, nwindows=9, margin=80, minpix=1, draw_windows=True):
     rightx = nonzerox[right_lane_inds]
     righty = nonzeroy[right_lane_inds]
 
+    leftDetected = False
+    rightDetected = False
+    direction = 0 # 1 : right, -1 : left
+    balanced = 0 # 1 : right, -1 : left
+    
+    """
+    ::TODO::
+    
+    left_fit[0]과 right_fit[0] 이 같은 부호라면 detected = 1 아니라면 0
+    
+    """
+    
     # Fit a second order polynomial to each
+    # left fit
     try:
-        # print("lefty :", lefty)
-        # print("leftx :", leftx)
-        # print("righty :", righty)
-        # print("rightx :", rightx)
         left_fit = np.polyfit(lefty, leftx, 2)
-        right_fit = np.polyfit(righty, rightx, 2)
-
+        
         left_a.append(left_fit[0])
         left_b.append(left_fit[1])
         left_c.append(left_fit[2])
+        
+        left_fit_[0] = np.mean(left_a[-10:])
+        left_fit_[1] = np.mean(left_b[-10:])
+        left_fit_[2] = np.mean(left_c[-10:])
+        
+        ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
+        
+        left_fitx = left_fit_[0]*ploty**2 + left_fit_[1]*ploty + left_fit_[2]
+        
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 100]
+        
+        leftDetected = True
+    except:
+        pass
+    
+    # right fit
+    try:
+        right_fit = np.polyfit(righty, rightx, 2)
 
         right_a.append(right_fit[0])
         right_b.append(right_fit[1])
         right_c.append(right_fit[2])
-
-        left_fit_[0] = np.mean(left_a[-10:])
-        left_fit_[1] = np.mean(left_b[-10:])
-        left_fit_[2] = np.mean(left_c[-10:])
-
+        
         right_fit_[0] = np.mean(right_a[-10:])
         right_fit_[1] = np.mean(right_b[-10:])
         right_fit_[2] = np.mean(right_c[-10:])
 
         # Generate x and y values for plotting
         ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
-        left_fitx = left_fit_[0]*ploty**2 + left_fit_[1]*ploty + left_fit_[2]
+        
         right_fitx = right_fit_[0]*ploty**2 + right_fit_[1]*ploty + right_fit_[2]
 
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 100]
-        out_img[nonzeroy[right_lane_inds],
-                nonzerox[right_lane_inds]] = [0, 100, 255]
+        out_img[nonzeroy[right_lane_inds],nonzerox[right_lane_inds]] = [0, 100, 255]
 
-        # print("left_fitx :", left_fitx)
-        # print("left_fitx :", len(left_fitx))
-        # print("right_fitx :", right_fitx)
-        # print("right_fitx :", len(right_fitx))
-        return out_img, (left_fitx, right_fitx), (left_fit_, right_fit_), ploty
+        rightDetected = True
     except:
-        ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
-        return out_img, (0, 0), (0, 0), ploty
+        pass
+        
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
+    
+    # print("lefty :", lefty)
+    # print("leftx :", leftx)
+    # print("righty :", righty)
+    # print("rightx :", rightx)
+        
+    # Which direction the curved lane made
+    if leftDetected and rightDetected:
+        if left_fit[0] * right_fit[0] > 0:
+            if left_fit[0] > 0:
+                direction = -1 
+            else:
+                direction = 1
+                
+        if 150 < max(lefty) < 160 and 150 < max(righty) < 160:
+            if abs(leftx[0] - (320 - rightx[0])) < 10:
+                pass
+            elif leftx[0] > (320 - rightx[0]):
+                balanced = 320 - rightx[0] # have to MOVE left
+            elif leftx[0] < (320 - rightx[0]):
+                balanced = -leftx[0] # have to MOVE right
+                
+        elif 150 < max(lefty) < 160:
+            balanced = -leftx[0]
+        elif 150 < max(righty) < 160:
+            balanced = 320 - rightx[0]
+        else:
+            if abs(max(lefty) - max(righty)) < 10:
+                pass
+            elif max(lefty) - max(righty) > 0:
+                balanced = -leftx[0]
+            elif max(lefty) - max(righty) < 0:
+                balanced = 320 - rightx[0]
+                
+        if max(leftx) - min(leftx) != 0 and max(rightx) - min(rightx) != 0:
+            a = (max(lefty) - min(lefty)) / (min(leftx) - max(leftx)) # + : right, - : wrong
+            b = (min(righty) - max(righty)) / (min(rightx) - max(rightx)) # + : right, - : wrong
+            
+            if a < b:
+                k = -a
+            else:
+                k = b
+                
+            if a != 0 and b != 0:
+                _balanced = 1 / b + 1 / a
+                # if a > b:
+                #     balanced = 1 / b - 1 / a
+                # else:
+                #     balanced = 1 / (b - a)
+            else:
+                _balanced = 0 
+        else:
+            k = 0
+            _balanced = 0 
+
+        balanced += 50 * _balanced
+            
+        return out_img, (left_fitx, right_fitx), (left_fit_, right_fit_), ploty, direction, balanced, "Both detected : {}".format(balanced) # "a : {:2f}\n b : {:2f}".format(float(a), float(b))
+    
+    elif leftDetected: # or rightDetected
+        if leftx[0] > leftx[-1]:
+            direction = -1
+        else:
+            direction = 1
+        
+        # if left_fit[0] > 0:
+        #     direction = -1
+        # else:
+        #     direction = 1
+            
+        if leftx[0] > 30:
+            balanced = -leftx[0]
+            
+        if max(leftx) - min(leftx) != 0:
+            k = (min(lefty) - max(lefty)) / (max(leftx) - min(leftx)) # + : right, - : wrong
+        else:
+            k = 0
+        
+        # if k < 0:
+        #     balanced = -balanced
+        
+        if k != 0:
+            _balanced = 1 / k
+        else:
+            _balanced = 0
+        
+        if k > 0:   
+            # _balanced = -k
+            balanced -= 100 * _balanced
+        else:
+            # _balanced = k
+            # balanced = 320 - leftx[0] - 100 * _balanced
+            # balanced = 320 - max(leftx) - 100 * _balanced
+            balanced = -85
+        
+        return out_img, (left_fitx, 0), (left_fit_, 0), ploty, direction, balanced, "Left detected : {}".format(balanced)
+    
+    elif rightDetected:
+        if rightx[0] < rightx[-1]:
+            direction = -1
+        else:
+            direction = 1
+        
+        if rightx[0] < 290:
+            balanced = 320 - rightx[0]
+        
+        if max(rightx) - min(rightx) != 0:
+            k = (min(righty) - max(righty)) / (min(rightx) - max(rightx)) # + : right, - : wrong
+        else:
+            k = 0
+        
+        if k != 0:
+            _balanced = 1 / k
+        else:
+            _balanced = 0
+        
+        if k > 0:   
+            # _balanced = k
+            balanced += 100 * _balanced
+        else:
+            # _balanced = k
+            # balanced = 320 - rightx[0] + 100 * _balanced
+            # balanced = 320 - min(rightx) + 100 * _balanced
+            balanced = 85
+        
+        return out_img, (0, right_fitx), (0, right_fit_), ploty, direction, balanced, "Right detected : {}".format(balanced)
+    else:
+        return out_img, (0, 0), (0, 0), ploty, direction, balanced, "Not detected"
+        
 
 
 def get_curve(img, leftx, rightx):
@@ -284,21 +424,21 @@ def get_curve(img, leftx, rightx):
             left_fit_cr[1]*img.shape[0] + left_fit_cr[2]
         r_fit_x_int = right_fit_cr[0]*img.shape[0]**2 + \
             right_fit_cr[1]*img.shape[0] + right_fit_cr[2]
-        print("car_pos :", car_pos)
-        print(l_fit_x_int, r_fit_x_int)
+        # print("car_pos :", car_pos)
+        # print(l_fit_x_int, r_fit_x_int)
         # center = (l_fit_x_int + r_fit_x_int) / 100
         
         lane_center_position = (r_fit_x_int + l_fit_x_int) / 2
         center = (car_pos - lane_center_position) * xm_per_pix / 10
         # Now our radius of curvature is in meters
         if center > 0:
-            print("LEFTLEFTLEFTLEFTLEFTLEFTLEFTLEFT")
+            # print("LEFTLEFTLEFTLEFTLEFTLEFTLEFTLEFT")
             direction = "LEFT"
         elif center < 0:
-            print("RIGHTRIGHTRIGHTRIGHTRIGHTRIGHTRIGHT")
+            # print("RIGHTRIGHTRIGHTRIGHTRIGHTRIGHTRIGHT")
             direction = "RIGHT"
         else:
-            print("CENTERCENTERCENTERCENTERCENTERCENTER")
+            # print("CENTERCENTERCENTERCENTERCENTERCENTER")
             direction = "CENTER"
         
         return (left_curverad, right_curverad, center, direction)
@@ -336,8 +476,8 @@ def keepCenter(center, now, file=None):
         
         
         # print("\n\n\narr[-1] : {}\n\n\n".format(normalized[0]))
-        print("mean :", np.mean(center))
-        print("std :", np.std(center))
+        # print("mean :", np.mean(center))
+        # print("std :", np.std(center))
         
         if file is not None:
             file.write("mean :{}\n".format(np.mean(center)))
@@ -345,7 +485,7 @@ def keepCenter(center, now, file=None):
         
         # normalized = normalize(arr)
         # if abs(normalized[0][-1]) > 0.0025:
-        if abs(center[-1] - now) > 0.20: # important
+        if abs(center[-1] - now) > 0.3: # important
                 # print("Im AAAAAAAAAAAAAAAA", center[-1])
                 return center[-1], -1
             
@@ -360,3 +500,78 @@ def keepCenter(center, now, file=None):
         else:
             return now, 1  # return normalized[-1], 1
 
+def keepBalance(balance, now, file=None):
+    if len(balance) == 1:
+        return now, 1
+    else:
+        # center.append(now)
+        # arr = np.array([center])
+        # arr = np.asarray(center, dtype=np.float16)    
+        
+        
+        # print("\n\n\narr[-1] : {}\n\n\n".format(normalized[0]))
+        # print("mean :", np.mean(balance))
+        # print("std :", np.std(balance))
+        
+        if file is not None:
+            file.write("mean :{}\n".format(np.mean(balance)))
+            file.write("std :{}\n\n".format(np.std(balance)))
+        
+        # normalized = normalize(arr)
+        # if abs(normalized[0][-1]) > 0.0025:
+        
+        # if abs(balance[-1] - now) < 50 and abs(now) > 95:
+        #     if now > 0:
+        #         return 100, 1
+        #     else:
+        #         return -100, 1
+        
+        # if balance[-1] * now < 0 and 30 < abs(balance[-1]) < 50:
+        #     return now, 1
+        
+        # if abs(balance[-1] - now) > 35: # and abs(now) > 25 and len(balance) > 500: important
+        #         # print("Im AAAAAAAAAAAAAAAA", center[-1])
+        #         return balance[-1], -1
+            
+        # balance = np.array(balance[-10:]) ########## modify ##########
+        # left = np.where(balance < -70) ########## modify ##########
+        # right = np.where(balance > 70) ########## modify ##########
+         
+        # print("balance :", balance)
+        # print("left :", np.shape(left))
+        # print("right :", np.shape(right))
+        # # print("len(left) :", left.shape)
+        # # print("len(right) :", right.shape)
+        
+        # if np.shape(left)[1] - np.shape(right)[1] >= 0 and np.shape(left)[1] > 3: ########## modify ##########
+        #     print("+++++++++++++++++++++++++++++++++++++++++")
+        #     temp = balance[left]
+            
+        #     if np.mean(temp) < -100:
+        #         return -99, 1
+        #     else:
+        #         return np.mean(temp), 1
+                
+        # elif np.shape(right)[1] - np.shape(left)[1] >= 0 and np.shape(right)[1] > 3: ########## modify ##########
+        #     print("-----------------------------------------")
+        #     temp = balance[right]
+            
+        #     if np.mean(temp) > 100:
+        #         return 99, 1
+        #     else:
+        #         return np.mean(temp), 1
+        # else:
+        #     return 0, 1
+            
+        ############################################################
+        if abs(now) > 100:
+            if now > 0:
+                # print("Im BBBBBBBBBBBBBBBB", 1)
+                # return center[-2], -1 # return normalized[-2], -1
+                return 99, 1
+            else:
+                # print("Im CCCCCCCCCCCCCCCC", -1)
+                return -99, 1
+        else:
+            return now, 1  # return normalized[-1], 1
+        ############################################################
